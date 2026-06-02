d92e19f8f8e1d2f0bc79f81d0c0e22a902e6747c
AwangDarmawan <adarmawan468@gmail.com>
2026-06-02 11:21:10 +0700

    musik

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,19 +7,31 @@ import Lovesection from "../components/Lovesection";
 import Giftsection from "../components/Giftsection";
 import Message from "../components/Message";
 import Mengundangsection from "../components/Mengundangsection";
-import  { useEffect, useRef } from "react";
+// import  { useEffect, useRef } from "react";
 import musik from "../assets/CINTA.mp3"
+import { useEffect, useRef, useState } from "react";
 
 
 export default function Home() {
   
+  // const audioRef = useRef(null);
   const audioRef = useRef(null);
+const [isPlaying, setIsPlaying] = useState(false);
 
+// useEffect(() => {
+//   const shouldPlay = localStorage.getItem("playMusic");
+
+//   if (shouldPlay === "true") {
+//     audioRef.current?.play().catch(() => {});
+//   }
+// }, []);
 useEffect(() => {
   const shouldPlay = localStorage.getItem("playMusic");
 
   if (shouldPlay === "true") {
-    audioRef.current?.play().catch(() => {});
+    audioRef.current?.play().then(() => {
+      setIsPlaying(true);
+    }).catch(() => {});
   }
 }, []);
   return (
@@ -33,7 +45,7 @@ useEffect(() => {
      <Giftsection/>
      <Message/>
      <Mengundangsection/>
-       <button
+       {/* <button
   onClick={() => {
     if (audioRef.current.paused) {
       audioRef.current.play();
@@ -51,13 +63,53 @@ useEffect(() => {
     w-12
     h-12
     rounded-full
-    bg-[#3E5C93]
+    border
+    border-[#3E5C93] 
     text-white
-    shadow-lg
+    shadow-2xl
   "
 >
   🎵
+</button> */}
+<button
+  onClick={() => {
+    if (audioRef.current.paused) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+  }}
+  className="
+    fixed
+    bottom-5
+    right-5
+    z-50
+    w-12
+    h-12
+    rounded-full
+    text-white
+    border
+   border-[#3E5C93] 
+    flex
+    items-center
+    justify-center
+     shadow-[0_4px_15px_rgba(62,92,147,0.5)]
+  "
+>
+  {isPlaying ? (
+    <span >🎵</span>
+  ) : (
+    <span className="relative text-xl">
+      🎵
+      <span className="absolute -top-1 -right-1  ">
+        ❌
+      </span>
+    </span>
+     )}
 </button>
+
      {/* musik */}
      <audio ref={audioRef} loop>
   <source src={musik} type="audio/mp3" />
